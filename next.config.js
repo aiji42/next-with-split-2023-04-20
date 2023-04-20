f7b82575824dbaa604aff7751d987821a92153d6
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-04-20 15:06:55 +0900

    start split test challenger-1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-const withSplit = require('next-with-split').withSplit({})
+const withSplit = require('next-with-split').withSplit({
+  splits: {
+    test1: {
+      path: '/*',
+      hosts: {
+        original: 'next-with-split-2023-04-20.vercel.app',
+        'challenger-1': 'next-with-split-2023-04-20-git-challenger-1-aiji42.vercel.app'
+      }
+    }
+  }
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
